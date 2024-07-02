613d896dd7d10f11612dc71f8dec8b1f102eec64
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-02 19:07:32 +0530

    updated transaction function

--- Bank_management/bank.c
+++ Bank_management/bank.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 char name[20];
-int acc_no, amt_dpt, amt = 1000, amt_with;
+int acc_no, amt_dpt, amt = 1000, amt_with, amt_transf;
 
 void deposit_money();
 void withdraw_money();
@@ -79,16 +79,21 @@ void deposit_money()
     time_t tm;
     time(&tm);
     FILE *ptr = fopen("Account.txt", "a");
-    printf("--DEPOSITING MONEY--\n");
+    if (ptr == NULL)
+    {
+        printf("Error opening file!\n");
+        return;
+    }
 
+    printf("--DEPOSITING MONEY--\n");
     printf("Enter the amount to be deposited: ");
     scanf("%d", &amt_dpt);
     amt += amt_dpt;
     printf("----Money Deposited----\n");
     printf("Current balance in your account is: %d\n", amt);
-    fprintf(ptr, "Rs%d has been deposited to Your account\n", amt_dpt);
-    fprintf(ptr, "Date and time of transcation %s\n", ctime(&tm));
-    getch();
+    fprintf(ptr, "Rs %d has been deposited to your account\n", amt_dpt);
+    fprintf(ptr, "Date and time of transaction: %s\n", ctime(&tm));
+    fclose(ptr);
 }
 
 void withdraw_money()
@@ -96,36 +101,82 @@ void withdraw_money()
     time_t tm;
     time(&tm);
     FILE *ptr = fopen("Account.txt", "a");
+    if (ptr == NULL)
+    {
+        printf("Error opening file!\n");
+        return;
+    }
+
     printf("--WITHDRAWING MONEY--\n");
-    printf("Enter the amount to withdraw :- ");
+    printf("Enter the amount to withdraw: ");
     scanf("%d", &amt_with);
     amt -= amt_with;
-    printf("AMOUNT WITHDRAWN \n");
+    printf("AMOUNT WITHDRAWN\n");
     printf("Current balance in your account is: %d\n", amt);
-    fprintf(ptr, "Rs %d has been credited from your account\n", amt_with);
-    fprintf(ptr, "Date and time of transcation %s\n", ctime(&tm));
-    getch();
+    fprintf(ptr, "Rs %d has been withdrawn from your account\n", amt_with);
+    fprintf(ptr, "Date and time of transaction: %s\n", ctime(&tm));
+    fclose(ptr);
 }
 
 void transfer_money()
 {
+    int ac;
+    time_t tm;
+    time(&tm);
+    FILE *ptr = fopen("Account.txt", "a");
+    if (ptr == NULL)
+    {
+        printf("Error opening file!\n");
+        return;
+    }
+
     printf("--TRANSFERRING MONEY--\n");
+    printf("Enter the amount to be transferred: ");
+    scanf("%d", &amt_transf);
+    printf("Enter the account number to transfer the amount to: ");
+    scanf("%d", &ac);
+    if (amt < amt_transf)
+    {
+        printf("Insufficient balance...!\n");
+    }
+    else
+    {
+        amt -= amt_transf;
+        printf("--AMOUNT TRANSFERRED--\n");
+        printf("Current balance in your account is: %d\n", amt);
+        fprintf(ptr, "Rs %d has been transferred to account %d\n", amt_transf, ac);
+        fprintf(ptr, "Date and time of transaction: %s\n", ctime(&tm));
+    }
+    fclose(ptr);
 }
 
 void account_details()
 {
     printf("--ACCOUNT DETAILS--\n");
-    // Implement the account_details function here
+    printf("Name: %s\n", name);
+    printf("Account Number: %d\n", acc_no);
+    printf("Current Balance: %d\n", amt);
 }
 
 void transaction_details()
 {
     printf("--TRANSACTION DETAILS--\n");
-    // Implement the transaction_details function here
+    char ch;
+    FILE *ptr = fopen("Account.txt", "r");
+    if (ptr == NULL)
+    {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    while ((ch = fgetc(ptr)) != EOF)
+    {
+        putchar(ch);
+    }
+    fclose(ptr);
 }
 
 void exit_details()
 {
     printf("--EXITING--\n");
-    // Implement any necessary exit details here
 }
